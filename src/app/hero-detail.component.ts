060c53df9afe3b9fe4dc365f9e311bf7e79d75a8
Won Wook SONG <wsong0512@gmail.com>
2017-07-25 01:52:14 +0900

    refactor hero object

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
 })
 export class HeroDetailComponent {
+  hero: Hero;
 }
