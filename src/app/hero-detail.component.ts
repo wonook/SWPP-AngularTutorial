ad457a469e7b90f34ca1f854abb50ad0e38f96f6
Won Wook SONG <wsong0512@gmail.com>
2017-07-25 01:54:45 +0900

    display refactored hero-detail part

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Hero } from './hero';
 
 @Component({
@@ -6,5 +6,5 @@ import { Hero } from './hero';
   templateUrl: './hero-detail.component.html',
 })
 export class HeroDetailComponent {
-  hero: Hero;
+  @Input() hero: Hero;
 }
