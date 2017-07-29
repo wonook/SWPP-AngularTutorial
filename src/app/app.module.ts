f354cc86abf7b667152fddcb01c9562c0758c100
Won Wook SONG <wsong0512@gmail.com>
2017-07-30 03:13:45 +0900

    add and enable router module, and create dashboard component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    RouterModule.forRoot([
+      {
+      	path: '',
+      	redirectTo: '/dashboard',
+      	pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+      	path: 'dashboard',
+      	component: DashboardComponent
+      },
+    ]),
   ],
   providers: [
   	HeroService,
