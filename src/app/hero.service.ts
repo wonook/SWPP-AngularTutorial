dcebe70eaaebf5c50cc3a4ead951838c4ed9bf02
Won Wook SONG <wsong0512@gmail.com>
2017-08-01 16:10:10 +0900

    add ability to update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,7 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'http://localhost:8000/api/heroes';  // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { }
 
@@ -20,11 +21,6 @@ export class HeroService {
                .catch(this.handleError);
   }
 
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
-
   getHero(id: number): Promise<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get(url)
@@ -32,6 +28,21 @@ export class HeroService {
       .then(response => response.json().data as Hero)
       .catch(this.handleError);
   }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+  
 
   // OPTIONAL METHOD
   getHeroesSlowly(): Promise<Hero[]> {
